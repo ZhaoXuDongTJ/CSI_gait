6d760eeb7066638ea47ad10250b6865a878e983c
ZhaoXuDongTJ <921982351@qq.com>
2018-05-25 00:02:56 +0800

    init
    创建项目

---
+++ apply_sm.m
@@ -0,0 +1,21 @@
+function ret = apply_sm(csi, sm)
+
+error(nargchk(2,2,nargin));
+
+[M, ~, S] = size(csi);
+if M == 1
+    ret = csi;
+    return;
+end
+
+%% Allocate return array
+ret = zeros(size(csi));
+
+%% Actually undo the input spatial mapping
+for i=1:S
+    t = squeeze(csi(:,:,i));
+    H = t.' * sm;
+    ret(:,:,i) = H.';
+end
+
+end
